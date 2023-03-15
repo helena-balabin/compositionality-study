a1f3277f1315d23133a577c26f0d71d686cb6cbc
helena-balabin <helena.balabin@kuleuven.be>
2023-03-15 10:41:37 +0100

    fix long docstring

--- src/compositionality_study/__init__.py
+++ src/compositionality_study/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-"""Code for the preparation and analysis of the planned functional MRI experiment for the "Natural Language Processing in the Human Brain" PhD project."""
+"""Code for the preparation and analysis of the planned functional MRI experiment."""
 
 from .api import *  # noqa
