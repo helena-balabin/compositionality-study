1ae50acf6ba6412bdf852272cb253fc5e58ba126
helena-balabin <helena.balabin@kuleuven.be>
2023-04-13 11:36:04 +0200

    WIP: Add EDA, correlate sentence length to dep parse tree depth

--- src/compositionality_study/eda/captions_statistics.py
+++ src/compositionality_study/eda/captions_statistics.py
@@ -77,7 +77,7 @@ def dep_parse_tree_depth_histogram(
     :return: None
     :rtype: None
     """
-    # Get the sentences and their lengths
+    # Get the sentences
     coco_vg_ds = datasets.load_from_disk(coco_vg_ds_dir)
     captions_nested = coco_vg_ds["sentences_raw"]
     captions = [sent for sent_list in captions_nested for sent in sent_list]
@@ -128,6 +128,59 @@ def walk_tree(
         return depth
 
 
+@click.command()
+@click.option("--coco_vg_ds_dir", type=str, default=VG_COCO_OVERLAP_DIR)
+@click.option("--vis_output_dir", type=str, default=VISUALIZATIONS_DIR)
+@click.option("--color", type=str, default="#9B9ED4")
+def sent_len_dep_depth_correlation(
+    coco_vg_ds_dir: str = VG_COCO_OVERLAP_DIR,
+    vis_output_dir: str = VISUALIZATIONS_DIR,
+    color: str = "#9B9ED4",
+):
+    """Plot a joint plot of the sentence lengths and dependency parse tree depths for all COCO captions.
+
+    :param coco_vg_ds_dir: The VG + COCO overlap dataset with the captions to plot the histogram for
+    :type coco_vg_ds_dir: str
+    :param vis_output_dir: Where to save the figures of the plots to, defaults to VISUALIZATIONS_DIR
+    :type vis_output_dir: str
+    :param color: Color of the bars in the histogram, defaults to "#66C2A5"
+    :type color: str
+    :return: None
+    :rtype: None
+    """
+    # Get the sentences
+    coco_vg_ds = datasets.load_from_disk(coco_vg_ds_dir)
+    captions_nested = coco_vg_ds["sentences_raw"]
+    captions = [sent for sent_list in captions_nested for sent in sent_list]
+
+    # Get their sentence lengths and dep parse tree depths
+    sent_lens = [len(sent.split()) for sent in captions]
+    # You might need to run `python -m spacy download en_core_web_sm` to download the model before
+    nlp = spacy.load("en_core_web_sm")
+    depths = []
+    for sentence in tqdm(captions, desc="Determing dependency parse tree depths"):
+        doc = nlp(sentence)
+        depths.append(walk_tree(next(doc.sents).root, 0))
+
+    # Set seaborn theme
+    plt.rcParams["font.family"] = ["sans-serif"]
+    sns.set_style("ticks")
+
+    # Create a joint plot
+    sns.jointplot(x=depths, y=sent_lens, cmap=sns.light_palette(color, as_cmap=True), kind="kde")
+    plt.title("VG + COCO overlap: \n Sentence length vs. dependency parse tree depth")
+    plt.xlabel("Dependency parse tree depth")
+    plt.ylabel("Sentence length")
+    plt.tight_layout()
+
+    # Create the output directory
+    vis_statistics_dir = os.path.join(vis_output_dir, "statistics")
+    os.makedirs(vis_statistics_dir, exist_ok=True)
+
+    # Save the figure
+    plt.savefig(os.path.join(vis_statistics_dir, "len_dep_correlation_plot.png"), dpi=300)
+
+
 @click.group()
 def cli() -> None:
     """Generate basic statistics on the VG + COCO overlap captions dataset."""
@@ -136,4 +189,5 @@ def cli() -> None:
 if __name__ == "__main__":
     cli.add_command(sent_len_histogram)
     cli.add_command(dep_parse_tree_depth_histogram)
+    cli.add_command(sent_len_dep_depth_correlation)
     cli()
