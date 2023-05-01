6f2c56d864c97e868ccd98920b76282ef815fe83
helena-balabin <helena.balabin@kuleuven.be>
2023-05-01 19:30:56 +0200

    Update the config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -230,8 +230,8 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
-    'numpy': ('https://numpy.org/doc/stable/', 'objects.inv'),
-    'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', 'objects.inv'),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', None),
     'scipy': ('https://docs.scipy.org/doc/scipy/', None),
 }
 
