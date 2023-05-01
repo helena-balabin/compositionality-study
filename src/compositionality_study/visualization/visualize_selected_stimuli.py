651702056e043e0e53feb6db256a84e54a526347
helena-balabin <helena.balabin@kuleuven.be>
2023-05-01 17:33:30 +0200

    Visualize the dependency trees of the stimuli using spacy_streamlit

--- src/compositionality_study/visualization/visualize_selected_stimuli.py
+++ src/compositionality_study/visualization/visualize_selected_stimuli.py
@@ -4,21 +4,46 @@ To see the stimuli, run the following command: streamlit run visualize_selected_
 """
 # Imports
 import requests
+import spacy
 import streamlit as st
 from datasets import load_from_disk
 from PIL import Image
+from spacy_streamlit import visualize_parser
 
 from compositionality_study.constants import VG_COCO_SELECTED_STIMULI_DIR
 
+# Set up the streamlit page
+st.set_page_config(layout="wide")
+
+# Load a spacy model to display the dependency trees
+nlp = spacy.load("en_core_web_sm")
+
 # Load the dataset
 dataset = load_from_disk(VG_COCO_SELECTED_STIMULI_DIR)
+# Get the complexities
+complexities = list(set([example["complexity"] for example in dataset]))
 
-# TODO visualize sorted by complexity
-# Iterate over the examples and display them
-for example in dataset:
-    # Display the image
-    try:
-        image = Image.open(requests.get(example["vg_url"], stream=True).raw)  # noqa
-        st.image(image, caption=example["sentences_raw"])
-    except:  # noqa
-        st.warning(f"Failed to load image for example with caption: {example['sentences_raw']}")
+# Create table-like layout
+col1, col2, col3, col4 = st.columns(4, gap="medium")
+
+for cell, comp in zip([col1, col2, col3, col4], complexities):
+    with cell:
+        header = comp.replace('_', ' ')  # noqa
+        st.header(f"{header}")
+        examples = [example for example in dataset if example["complexity"] == comp]
+        for example in examples:
+            try:
+                image = Image.open(requests.get(example["vg_url"], stream=True).raw)  # noqa
+                st.image(image, caption=f"{example['sentences_raw']}")
+                doc = nlp(example['sentences_raw'])
+                st.empty()
+                # Generate the dependency parse tree and display it
+                visualize_parser(
+                    doc,
+                    key=f"{example['cocoid']}_parser_split_sents",
+                    displacy_options={"compact": True, "distance": 65},
+                    title=None,
+                )
+            except:  # noqa
+                st.warning(f"Failed to load image for example with caption: {example['sentences_raw']}")
+            st.divider()
