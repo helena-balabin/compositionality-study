4f4e56f58856c89de0c32a318b4f4707a6732e14
helena-balabin <helena.balabin@kuleuven.be>
2023-08-02 15:44:48 +0200

    Split up the stimulus presentation and stimulus conversion steps

--- src/compositionality_study/experiment/generate_psychopy_files.py
+++ src/compositionality_study/experiment/generate_psychopy_files.py
@@ -1,174 +1,21 @@
 """Generate the psychopy experiment files for the fMRI experiment based on the chosen stimuli."""
 import os
 import random
-import string
 from typing import List
 
 # Imports
 import click
 import pandas as pd
 from psychopy import core, visual
-import requests
 from PIL import Image, ImageOps
-from datasets import load_from_disk
 from tqdm import tqdm
 
 from compositionality_study.constants import (
     VG_COCO_LOCAL_STIMULI_DIR,
-    VG_COCO_SELECTED_STIMULI_DIR,
 )
-from compositionality_study.experiment.create_fourier_scrambled_images import fft_phase_scrambling
 
 # Set a random seed for reproducibility
 random.seed(42)
-
-
-def estimate_letter_frequency(
-    text_series: pd.Series,
-) -> List[float]:
-    """Estimate the frequency of each English letter based on a series of text.
-
-    :param text_series: The series of text to use.
-    :type text_series: pd.Series
-    :return: The estimated letter frequencies.
-    :rtype: List[float]
-    """
-    # Concatenate all the text
-    text = " ".join(text_series.values)
-
-    # Count the number of occurrences of each letter
-    letter_counts = [text.count(letter) for letter in string.ascii_lowercase]
-
-    # Return the letter frequencies
-    return [count / sum(letter_counts) * 100 for count in letter_counts]
-
-
-def generate_non_word_sentence(
-    input_sentence: str,
-    letter_frequencies: List[float],
-) -> str:
-    """Generate nonword from a sentence by sampling letters according to their frequency.
-
-    :param input_sentence: The input sentence.
-    :type input_sentence: str
-    :param letter_frequencies: The letter frequencies to use.
-    :type letter_frequencies: List[float]
-    :return: The pseudoword sentence.
-    :rtype: str
-    """
-    # Split the sentence into words
-    words = input_sentence.split(" ")
-
-    # For each word, generate a non word of the same length
-    non_words = []
-    for word in words:
-        # Sample letters according to their frequency
-        letters = random.choices(string.ascii_lowercase, weights=letter_frequencies, k=len(word))
-
-        # Add the non word to the list
-        non_words.append("".join(letters))
-
-    # Return the non word sentence
-    return " ".join(non_words)
-
-
-@click.command()
-@click.option("--hf_stimuli_dir", default=VG_COCO_SELECTED_STIMULI_DIR, type=str)
-@click.option("--local_stimuli_dir", default=VG_COCO_LOCAL_STIMULI_DIR, type=str)
-@click.option("--delete_existing", default=True, type=bool)
-@click.option("--random_seed", default=42, type=int)
-def convert_hf_dataset_to_local_stimuli(
-    hf_stimuli_dir: str = VG_COCO_SELECTED_STIMULI_DIR,
-    local_stimuli_dir: str = VG_COCO_LOCAL_STIMULI_DIR,
-    delete_existing: bool = True,
-    random_seed: int = 42,
-) -> pd.DataFrame:
-    """Convert the stimuli from the huggingface dataset to locally stimuli (images/text).
-
-    :param hf_stimuli_dir: The directory containing the stimuli from the huggingface dataset.
-    :type hf_stimuli_dir: str
-    :param local_stimuli_dir: The directory to save the locally stimuli to.
-    :type local_stimuli_dir: str
-    :param delete_existing: Whether to delete the existing stimuli in the local stimuli directory.
-    :type delete_existing: bool
-    :param random_seed: The random seed to use for reproducibility.
-    :type random_seed: int
-    :return: A dataframe containing the text and path to the image and image ID.
-    :rtype: pd.DataFrame
-    """
-    # Load the dataset
-    dataset = load_from_disk(hf_stimuli_dir)
-    # Initialize a dataframe that contains the text, path to the image, image ID and the condition
-    stimuli_df = pd.DataFrame(columns=["text", "img_path", "img_id", "complexity"])
-
-    # Create the output directory if it does not exist
-    if not os.path.exists(local_stimuli_dir):
-        os.makedirs(local_stimuli_dir)
-    # Delete the existing stimuli if specified
-    if delete_existing:
-        for f in os.listdir(local_stimuli_dir):
-            os.remove(os.path.join(local_stimuli_dir, f))
-
-    # Iterate through the dataset
-    for ex in tqdm(dataset, desc="Downloading images"):
-        # Download and save the image
-        img = Image.open(requests.get(ex["vg_url"], stream=True).raw)
-        output_path = f"{ex['vg_image_id']}_{ex['sentids']}_{ex['complexity']}.jpg"
-        img.save(os.path.join(local_stimuli_dir, output_path))
-        # Add the text and image path to the dataframe
-        stimuli_df = pd.concat(
-            [
-                stimuli_df,
-                pd.DataFrame(
-                    {
-                        "text": ex["sentences_raw"],
-                        "img_path": output_path,
-                        "img_id": f"{ex['vg_image_id']}_{ex['sentids']}",
-                        "complexity": ex["complexity"],
-                    },
-                    index=[0],
-                ),
-            ],
-            ignore_index=True,
-        )
-
-    # Generate null condition stimuli by scrambling half of the images for each complexity condition
-    # Take a subset of the stimuli, stratified by complexity
-    stimuli_df_subset = stimuli_df.groupby("complexity").sample(frac=0.5, random_state=random_seed)
-
-    # Estimate the letter frequencies
-    letter_frequencies = estimate_letter_frequency(stimuli_df_subset["text"])
-
-    for ex in tqdm(stimuli_df_subset.itertuples(), desc="Generating null condition stimuli"):
-        # Create a scrambled version of the image and turn it into a PIL image
-        scrambled_img = Image.fromarray(fft_phase_scrambling(os.path.join(local_stimuli_dir, ex.img_path)))
-        sc_img_output_path = f"scrambled_{ex.img_id}.jpg"
-        # Write the scrambled image to disk
-        scrambled_img.save(os.path.join(local_stimuli_dir, sc_img_output_path))
-
-        # Generate a scrambled (non word) sentence as well
-        non_word_sentence = generate_non_word_sentence(ex.text, letter_frequencies)
-        # Add the info to the dataframe
-        stimuli_df = pd.concat(
-            [
-                stimuli_df,
-                pd.DataFrame(
-                    {
-                        "text": non_word_sentence,
-                        "img_path": sc_img_output_path,
-                        "img_id": ex.img_id,
-                        "complexity": f"scrambled_{ex.complexity}",
-                    },
-                    index=[0],
-                ),
-            ],
-            ignore_index=True,
-        )
-
-    # Save the dataframe and return it
-    stimuli_df.to_csv(os.path.join(local_stimuli_dir, "stimuli_text_and_im_paths.csv"), index=False)
-
-    return stimuli_df
 
 
 def run_images(
@@ -253,10 +100,9 @@ def generate_psychopy_exp(
 
 @click.group()
 def cli() -> None:
-    """Generate the psychopy experiment files for the fMRI experiment based on the chosen stimuli."""
+    """Generate the psychopy experiment files for the fMRI experiment based on the local chosen stimuli."""
 
 
 if __name__ == "__main__":
     cli.add_command(generate_psychopy_exp)
-    cli.add_command(convert_hf_dataset_to_local_stimuli)
     cli()
