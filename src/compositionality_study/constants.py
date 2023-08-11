ed0cbc538463f61ed9d59f7e7c4556f4bf97c113
helena-balabin <helena.balabin@kuleuven.be>
2023-08-11 17:09:31 +0200

    Introduce a constant for the preprocessed_all dataset

--- src/compositionality_study/constants.py
+++ src/compositionality_study/constants.py
@@ -52,6 +52,7 @@ VG_COCO_OVERLAP_DIR = os.path.join(VG_DIR, "vg_coco_overlap")
 VG_COCO_PREPROCESSED_TEXT_DIR = os.path.join(VG_DIR, "vg_coco_preprocessed_text")
 VG_COCO_PREP_TEXT_GRAPH_DIR = os.path.join(VG_DIR, "vg_coco_preprocessed_graph")
 VG_COCO_PREP_TEXT_IMG_SEG_DIR = os.path.join(VG_DIR, "vg_coco_preprocessed_img_seg")
+VG_COCO_PREP_ALL = os.path.join(VG_DIR, "vg_coco_preprocessed_all")
 VG_COCO_SELECTED_STIMULI_DIR = os.path.join(VG_DIR, "vg_coco_80_stimuli")
 # Directory for stimuli converted into local files for psychopy
 VG_COCO_LOCAL_STIMULI_DIR = os.path.join(VG_DIR, "vg_coco_local_stimuli")
