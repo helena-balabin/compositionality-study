c86a79d7567730cdfe89ab629c29e38af081f7a7
helena-balabin <helena.balabin@kuleuven.be>
2023-10-09 17:28:04 +0200

    Fix issue in how to include a minimum number of action verbs

--- src/compositionality_study/data/select_stimuli.py
+++ src/compositionality_study/data/select_stimuli.py
@@ -199,10 +199,12 @@ def select_stimuli(
     )
 
     # 5. Select by number action verbs that match max and min quantiles (min 1 action verb)
-    ac_min = max(1, int(pd.Series(vg_ds["n_rel_action_verbs"]).quantile(action_verbs_quantile)))
+    ac_min = int(pd.Series(vg_ds["n_rel_action_verbs"]).quantile(action_verbs_quantile))
     ac_max = int(pd.Series(vg_ds["n_rel_action_verbs"]).quantile(1 - action_verbs_quantile))
     vg_ds = vg_ds.filter(
-        lambda x: x["n_rel_action_verbs"] <= ac_min or x["n_rel_action_verbs"] >= ac_max,
+        lambda x: x["n_rel_action_verbs"] > 0 and (
+            x["n_rel_action_verbs"] <= ac_min or x["n_rel_action_verbs"] >= ac_max
+        ),
         num_proc=24,
     )
     logger.info(
