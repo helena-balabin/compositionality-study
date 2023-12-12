d3e71a34f0d882a97c645abd8b637b8f545f9ca6
helena-balabin <helena.balabin@kuleuven.be>
2023-12-12 16:52:01 +0100

    Start counting from one

--- src/compositionality_study/experiment/mri/generate_psychopy_files.py
+++ src/compositionality_study/experiment/mri/generate_psychopy_files.py
@@ -141,7 +141,7 @@ def run_single_run(
 @click.command()
 @click.option("--local_stimuli_dir", type=str, default=VG_COCO_LOCAL_STIMULI_DIR)
 @click.option("--practice_stimuli_dir", type=str, default=VG_COCO_PRACTICE_STIMULI_DIR)
-@click.option("--subject_id", type=int, default=0)
+@click.option("--subject_id", type=int, default=1)
 @click.option("--duration", type=float, default=3.0)
 @click.option("--inter_stimulus_interval", type=float, default=5.5)
 @click.option("--dummy_scan_duration", type=float, default=6.0)
@@ -153,7 +153,7 @@ def run_single_run(
 def run_psychopy_exp(
     local_stimuli_dir: str = VG_COCO_LOCAL_STIMULI_DIR,
     practice_stimuli_dir: str = VG_COCO_PRACTICE_STIMULI_DIR,
-    subject_id: int = 0,
+    subject_id: int = 1,
     duration: float = 3.0,
     inter_stimulus_interval: float = 5.5,
     dummy_scan_duration: float = 6.0,
